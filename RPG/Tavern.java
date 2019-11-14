7eae8a00b9c10dc691446194622abaef503c4bb3
samkitsheth95 <samkitraseshbhai.sheth@sjsu.edu>
2019-11-13 23:15:16 -0800

    Tavern and monster screen

---
+++ RPG/Tavern.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Tavern here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Tavern extends Actor
+{
+    /**
+     * Act - do whatever the Tavern wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        // Add your action code here.
+    }   
+}
