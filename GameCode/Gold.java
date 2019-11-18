fd968e7799beddd44e6b0283ef766851c870e0d0
AISHWARYA DEEP RASTOGI <aishwaryaicerastogi@gmail.com>
2019-11-18 01:32:48 -0800

    fixed bugs in demon's interaction with man and new gold sprite

--- GameCode/Gold.java
+++ GameCode/Gold.java
@@ -12,6 +12,12 @@ public class Gold extends Actor
      * Act - do whatever the Gold wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    Gold(){
+    GreenfootImage img = new GreenfootImage("gold.png");
+        
+        img.scale(60,60);
+        this.setImage(img);
+    }
     public void act() 
     {
         // Add your action code here.
