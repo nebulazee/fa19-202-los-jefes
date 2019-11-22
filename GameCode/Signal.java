af83c75684c495d015148f59ece4bed06774b20c
AISHWARYA DEEP RASTOGI <aishwaryaicerastogi@gmail.com>
2019-11-22 14:12:21 -0800

    enumerations class

---
+++ GameCode/Signal.java
@@ -0,0 +1,10 @@
+/**
+ * Write a description of class Signal here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public enum Signal  
+{
+   LEFT,RIGHT,UP,DOWN,NORTH,SOUTH,EAST,WEST,R,L,B,T,X;
+}
