5c664a3733c8d072046cb9a9c3554cade6df5682
varshitshah <varshitraju.shah@sjsu.edu>
2019-11-22 21:38:52 -0800

    Treasure Factory finalization, concrete command for treasures and weapons

--- GameCode/EndScreenText.java
+++ GameCode/EndScreenText.java
@@ -1,45 +1,43 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class EndScreenText here.
  * 
- * @author (your name) 
+ * @author (your name)
  * @version (a version number or a date)
  */
-public class EndScreenText extends Actor
-{
+public class EndScreenText extends Actor {
     /**
-     * Act - do whatever the EndScreenText wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
+     * Act - do whatever the EndScreenText wants to do. This method is called
+     * whenever the 'Act' or 'Run' button gets pressed in the environment.
      */
-     GreenfootImage vic,def;  
-     Scoreboard sb;
-     int health,monsterkill;
+    GreenfootImage vic, def;
+    Scoreboard sb;
+    int health, monsterkill;
     EndScreen es;
-    public EndScreenText()
-    {
-        if(WorldManager.getCurrentWorld() instanceof MonsterWorld){
-         sb = Scoreboard.getScoreboardInstance();  
-         health = sb.gethealth();
-         monsterkill = sb.getmonsterkill();
-         vic = new GreenfootImage("victoryedited.jpg");
-         def = new GreenfootImage("defeat.png"); 
-         if(health<=0)
-         {
-          setImage(def);
-         }else{
-           setImage(vic);      
-         }
+
+    public EndScreenText() {
+        if (WorldManager.getCurrentWorld() instanceof MonsterWorld) {
+            sb = Scoreboard.getScoreboardInstance();
+            health = sb.gethealth();
+            monsterkill = sb.getMonsterKill();
+            vic = new GreenfootImage("victoryedited.jpg");
+            def = new GreenfootImage("defeat.png");
+            if (health <= 0) {
+                setImage(def);
+            } else {
+                setImage(vic);
+            }
         }
     }
-    public void act() 
-    {
+
+    public void act() {
         // Add your action code here.
-        //if(sb.getMonsterKillCount()==3)
-        //{
-            //Greenfoot.setWorld(es);
-            //Greenfoot.start();
-       // }
-        
-    }    
+        // if(sb.getMonsterKillCount()==3)
+        // {
+        // Greenfoot.setWorld(es);
+        // Greenfoot.start();
+        // }
+
+    }
 }
--- GameCode/Treasure.java
+++ GameCode/Treasure.java
@@ -1,109 +1,124 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import greenfoot.*; // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
  * Write a description of class Treasure here.
  * 
- * @author (your name) 
+ * @author (your name)
  * @version (a version number or a date)
  */
-public abstract class Treasure extends GameActor
-{
+public abstract class Treasure extends GameActor {
     String imageClosed, imageOpen;
     private int strength;
     private int openDelay;
-    private boolean pickedWeapon;
+    private boolean openedChest;
     public Weapon associatedWeapon;
-    
-    Treasure()
-    {
+
+    Treasure() {
         super();
     }
-    
-    Treasure(String imageClosed, String imageOpen) 
-    {
+
+    Treasure(String imageClosed, String imageOpen) {
         super();
         this.imageClosed = imageClosed;
         this.imageOpen = imageOpen;
-        
+
+        setActorTitle("TREASURE CHEST");
+
         createWeapon();
-        
+
         GreenfootImage treasureClosedImage = new GreenfootImage(imageClosed);
-        treasureClosedImage.scale(50,50);
+        treasureClosedImage.scale(50, 50);
         setImage(treasureClosedImage);
 
         this.strength = 30;
         this.openDelay = 0;
-        //this.pickedWeapon = false;
+        this.openedChest = false;
     }
+
     class MotionRenderer {
         String file;
         GreenfootImage image;
-        public MotionRenderer (String file){
-            this.file=file;
-            this.image= new GreenfootImage(file);
-            //image.scale(60,60);
+
+        public MotionRenderer(String file) {
+            this.file = file;
+            this.image = new GreenfootImage(file);
+            // image.scale(60,60);
         }
     }
-    
+
     MotionRenderer im;
-    public void takeDamage()
-    {
-        this.strength = this.strength-10;
-            // System.out.println(this.strength);
+
+    public void takeDamage() {
+        this.strength = this.strength - 10;
+        // System.out.println(this.strength);
     }
-    
-    public Weapon exchangeWeapon(Weapon w)
-    {
-        //this.pickedWeapon = true;
+
+    public Weapon exchangeWeapon(Weapon w) {
         Weapon temp = associatedWeapon;
         this.associatedWeapon = w;
-        Scoreboard.updateWeapon(1);
         updateWeaponImage();
-        
+        setActorTitle(w.getWeaponStats());
+
         return temp;
 
     }
-    
-    private void updateWeaponImage()
-    {
+
+    private void updateWeaponImage() {
         GreenfootImage weaponImage = new GreenfootImage(associatedWeapon.getWeaponImage());
+        weaponImage.scale(60, 60);
         setImage(weaponImage);
     }
 
     public abstract void createWeapon();
-    
-    public void act() 
-    {
-        
-        if(this.strength <= 0)
-        {
+
+    public void act() {
+
+        if (this.strength <= 0) {
             this.openDelay++;
-            if(this.openDelay == 60)
-            {
+            if (this.openDelay == 60) {
                 GreenfootImage treasureOpenImage = new GreenfootImage(imageOpen);
-                treasureOpenImage.scale(50,50);
+                treasureOpenImage.scale(50, 50);
                 setImage(treasureOpenImage);
-                //this.setImage(new MotionRenderer("treasure-open-blue.png").image);
-            }
-            else if(this.openDelay == 120)
-            {
-                GreenfootImage weaponImage = new GreenfootImage(associatedWeapon.getWeaponImage());
-                setImage(weaponImage);
-                //this.setImage(new MotionRenderer("blue-sword.png").image);
+            } else if (this.openDelay == 120) {
+                chestOpened();
             }
         }
-        
-    }  
-    public void setImage(MotionRenderer img){
-    //System.err.println("Man image is set to "+img.fileName);
+
+    }
+
+    private void chestOpened() {
+
+        this.openedChest = true;
+        updateWeaponImage();
+
+        setActorTitle(associatedWeapon.getWeaponStats());
+
+        commandMap.remove("A");
+        IPlayerCommandTarget pickWeaponCmd = new IPlayerCommandTarget() {
+            public void act(Man player) {
+                Weapon picked = exchangeWeapon(Scoreboard.getCurrentWeapon());
+                Scoreboard.updateWeapon(picked);
+            }
+        };
+        newCommandBinding("Q", "Swap with this weapon", pickWeaponCmd);
+    }
+
+    public void setImage(MotionRenderer img) {
+        // System.err.println("Man image is set to "+img.fileName);
         super.setImage(img.image);
     }
-    
-    //public abstract Treasure getTreasure();
-    
-    
-    
-    
-    
-    
+
+    public void createCommandBindings() {
+
+        IPlayerCommandTarget openChestCmd = new IPlayerCommandTarget() {
+            public void act(Man player) {
+                takeDamage();
+            }
+        };
+        newCommandBinding("A", "Attack to break open!", openChestCmd);
+
+    }
+
+    // public abstract Treasure getTreasure();
+
 }
